5ee9be49bca206203283e19a3d1faab1f627d28a
arinaleech <ndjdn6062@gmail.com>
2025-01-04 14:20:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ if __name__ == "__main__" :
 
 
     user = Client(
-        "User",
+        "karan367",
         session_string=Config.SESSION_STR,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH
